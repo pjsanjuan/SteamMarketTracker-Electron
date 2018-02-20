70e630a7fd114d5d9732d3777333f662d9dbfede
Patrick San Juan <psanjuan06.sj@gmail.com>
2018-02-20 12:10:07 -0800

    Synchronous fetching of items from json file

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@
 <script>
 import sidebar from "./Sidebar.vue";
 import tracker from "./Tracker.vue";
-// import fse from "fs-extra";
-// var ipcRenderer = require("electron").ipcRenderer;
+var ipcRenderer = require("electron").ipcRenderer;
 
 export default {
   components: {
@@ -25,20 +24,7 @@ export default {
   },
   name: "app",
   created: function() {
-    var tempItems = JSON.parse(
-      '{"items":[{"url":"http://steamcommunity.com/market/listings/730/Clutch%20Case","buyPrice":10.2},{"url":"http://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29","buyPrice":10.7},{"url":"http://steamcommunity.com/market/listings/730/AK-47%20%7C%20Frontside%20Misty%20%28Field-Tested%29","buyPrice":3.5}]}'
-    );
-    this.$store.commit('setItems', tempItems)
-    console.log(this.$store.getters.getItems)
+    this.$store.commit("setItems", ipcRenderer.sendSync("fetchItemJson-sync"));
   }
-  //   created: function() {
-  //     var store = this.$store;
-  //     store = JSON.parse('{"items":[{"url":"http://steamcommunity.com/market/listings/730/Clutch%20Case","buyPrice":10.2},{"url":"http://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29","buyPrice":10.7},{"url":"http://steamcommunity.com/market/listings/730/AK-47%20%7C%20Frontside%20Misty%20%28Field-Tested%29","buyPrice":3.5}]}'
-  // )
-  //     // ipcRenderer.on("itemJson", itemJson => {
-  //     //   var savedItems = fse.readJsonSync(itemJson);
-  //     //   $store.commit("setItems", items);
-  //     // });
-  //   }
 };
 </script>
